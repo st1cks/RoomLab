252764d458fa0839f28a7c493c8a28688745d44b
BT_1N3_06 <rzhao3818@bths.edu>
2018-12-05 14:54:16 -0500

    working on it

---
+++ src/Enemies/EnemyTypes.java
@@ -0,0 +1,14 @@
+package Enemies;
+
+public class EnemyTypes {
+    private String Goblin;
+    private String Orc;
+    private String Slime;
+
+    public EnemyTypes(){
+        this.Goblin = Goblin;
+        this.Orc = Orc;
+        this.Slime = Slime;
+    }
+
+}
