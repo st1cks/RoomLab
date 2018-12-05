7faf3c59100673dcabab33963f89832e9223f412
BT_1N3_06 <rzhao3818@bths.edu>
2018-12-05 15:29:30 -0500

    working on it

--- src/Enemies/Goblin.java
+++ src/Enemies/Goblin.java
@@ -1,4 +1,22 @@
 package Enemies;
 
-public class Goblin {
+public class Goblin extends Enemy{
+    private int HP;
+    private int ATK;
+    private int droppedGold;
+    private String totalStats;
+
+    public Goblin(){
+        this.HP = HP;
+        this.ATK = ATK;
+       this.droppedGold = droppedGold;
+       this.totalStats = totalStats;
+    }
+
+    public String GoblinHP(){
+        HP = 10;
+        ATK = 3;
+        totalStats = "HP = "+HP+"";
+        return totalStats;
+    }
 }
