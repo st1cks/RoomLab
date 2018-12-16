178a86b6cc50de935d2c238a33b98c26027465e7
Ricky zhao <rzhao3818@bths.edu>
2018-12-16 15:59:29 -0500

    Working on it

---
+++ src/Enemies/Enemy1.java
@@ -0,0 +1,33 @@
+package Enemies;
+
+public class Enemy1 implements Enemy {
+
+    public String getName(){
+        return "Chad the Business Man";
+    }
+
+    public void printEnemy(){
+        System.out.println ("    /:''|    ");
+        System.out.println ("   |: 66|_   ");
+        System.out.println ("   C     _)  ");
+        System.out.println ("    \\ ._|      ");
+        System.out.println ("     ) /       ");
+        System.out.println ("   /`\\       ");
+        System.out.println ("   || |Y|       ");
+        System.out.println ("   || |#|       ");
+        System.out.println ("   || |#|       ");
+        System.out.println ("   || |#|       ");
+        System.out.println ("   :| |=:       ");
+        System.out.println ("   ||_|,|      ");
+        System.out.println ("   \\)))||     ");
+        System.out.println ("|~~~`-`~~~|   ");
+        System.out.println ("|         |    ");
+        System.out.println ("|_________|    ");
+        System.out.println ("|_________|    ");
+        System.out.println ("    | ||       ");
+        System.out.println ("    |_||__        ");
+        System.out.println ("    (____))    ");
+    }
+
+
+}
